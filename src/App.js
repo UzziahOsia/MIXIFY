5a71a72a29dfb7223f8f32a7828b376f48b6400e
Uzziah Osia <85574539+UzziahOsia@users.noreply.github.com>
2023-04-11 15:12:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,55 @@ import React from "react";
 import { useState } from "react";
 
 var musicDB = {
-  All: [
+  Continue: [
+    All: [
+    { name: "Joha - Asake", rating: "5/5", url: ""},
+    { name: "Dull - Asake", rating: "", url: ""},
+    { name: "Yoga - Asake", rating: "5/5", url: ""},
+    { name: "Sungba - Asake", rating: "4/5", url: ""},
+    { name: "Nzaza - Asake", rating: "4/5", url: ""},
+    { name: "Unavailable - Davido", rating: "5/5", url: ""},
+    { name: "Over Dem - Davido", rating: "5/5", url: ""},
+    { name: "Feel - Davido", rating: "4/5", url: ""},
+    { name: "Away - Davido", rating: "4/5", url: ""},
+    { name: "Godfather - Davido", rating: "4/5", url: ""},
+    { name: "Kante - Davido", rating: "4/5", url: ""},
+    { name: "Bloody Samaritan - Ayra Starr", rating: "5/5", url: ""},
+    { name: "Rush - Ayra Starr", rating: "4/5", url: ""},
+    { name: "Sabilty - Ayra Starr", rating: "4/5", url: ""},
+    { name: "Common Person - Burna Boy", rating: "5/5", url: ""},
+    { name: "For my Hand - Burna Boy", rating: "4/5", url: ""},
+    { name: "Last Last - Burna Boy", rating: "4/5", url: ""},
+    { name: "Way Too Big - Burna Boy", rating: "5/5", url: ""},
+    { name: "Anybody - Burna Boy", rating: "5/5", url: ""}
   ],
   Asake: [
+    { name: "Joha", rating: "5/5", url: ""},
+    { name: "Dull", rating: "", url: ""},
+    { name: "Yoga", rating: "5/5", url: ""},
+    { name: "Sungba", rating: "4/5", url: ""},
+    { name: "Nzaza", rating: "4/5", url: ""}
   ],
   Davido: [
+    { name: "Unavailable", rating: "5/5", url: ""},
+    { name: "Over Dem", rating: "5/5", url: ""},
+    { name: "Feel", rating: "4/5", url: ""},
+    { name: "Away", rating: "4/5", url: ""},
+    { name: "Godfather", rating: "4/5", url: ""},
+    { name: "Kante", rating: "4/5", url: ""}
   ],
   AyraStarr: [
+    { name: "Bloody Samaritan", rating: "5/5", url: ""},
+    { name: "Rush", rating: "4/5", url: ""},
+    { name: "Sabilty", rating: "4/5", url: ""}
   ],
   BurnaBoy: [
+    { name: "Common Person", rating: "5/5", url: ""},
+    { name: "For my Hand", rating: "4/5", url: ""},
+    { name: "Last Last", rating: "4/5", url: ""},
+    { name: "Way Too Big", rating: "5/5", url: ""},
+    { name: "Anybody", rating: "5/5", url: ""}
+  ],
   ],
 };
 
