58bb628acea6d4fed2ecd42b98084d34851aa9be
Uzziah Osia <85574539+UzziahOsia@users.noreply.github.com>
2023-04-12 10:55:47 +0100

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,27 @@ import "./Home.css";
 import React from "react";
 
 const Home = () => (
-  <div>
+  <div><div style={{ textAlign: "center" }}>
+  <ul style={{ paddingInlineStart: "0" }}>
+    {musicDB[selectedArtist].map((song) => (
+      <li
+      key={song.name}
+      style={{
+        listStyle: "none",
+        padding: "1rem",
+        border: "2px solid #065F46",
+        width: "70%",
+        margin: "1rem 0rem",
+        borderRadius: "0.7rem",
+        background: "#ECFDF5"
+      }}
+      >
+        <a href={song.url} style={{ fontSize: "large", color: "#ECFDF5" }}>{song.name}</a>
+        <div style={{ fontSize: "large" }}>{song.rating}</div>
+      </li>
+    ))}
+  </ul>
+</div>
     <h1 style={{
             cursor: "pointer",
             background: "#059669",
@@ -22,10 +42,10 @@ const Home = () => (
             border: "2px solid #065F46",
             margin: "1rem 0.3rem",
             color: "white"
-            }}>Discover your next favorite song with our music recommender app. 
-            Our algorithm analyzes your listening habits and suggests new tracks tailored to your tastes. 
-            With a vast library of songs from different genres and eras, you'll never run out of options. 
-            Save your favorite tracks and create playlists for any occasion. Start exploring new music today with our app.
+            }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida,
+      risus at dapibus aliquet, elit quam scelerisque tortor, nec accumsan eros
+      nulla interdum justo. Pellentesque dignissim, sapien et congue rutrum,
+      lorem tortor dapibus turpis, sit amet vestibulum eros mi et odio.
     </p>
   </div>
 );
