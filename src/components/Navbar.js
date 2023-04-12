7c4674ed195b40f4b328def969f9fb4152c4802b
Uzziah Osia <85574539+UzziahOsia@users.noreply.github.com>
2023-04-12 11:18:03 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,6 @@
 import React, { useState } from "react";
 import { NavLink, withRouter } from "react-router-dom";
 
-const Navbar = ({ history }) => {
-  const [isOpen, setOpen] = useState(false);
-
-  const isAuth = !!localStorage.getItem("token");
-
-  const loginUser = () => {
-    localStorage.setItem("token", "some-login-token");
-    history.push("/profile/Vijit");
-  };
-
-  const logoutUser = () => {
-    localStorage.removeItem("token");
-    history.push("/");
-  };
-
-  return (
-    <nav
-      className="navbar is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <a
-            role="button"
-            className={`navbar-burger burger ${isOpen && "is-active"}`}
-            aria-label="menu"
-            aria-expanded="false"
-            onClick={() => setOpen(!isOpen)}
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
-
         <div className={`navbar-menu ${isOpen && "is-active"}`}>
           <div className="navbar-start">
             <NavLink
