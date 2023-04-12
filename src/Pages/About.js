7f831a38e24b458118543fc2e141f21ffb13ef22
Uzziah Osia <85574539+UzziahOsia@users.noreply.github.com>
2023-04-12 14:43:14 +0100

    Added directory link

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -5,7 +5,7 @@ const About = () => (
     <h1 className="title is-1">This is the About Page</h1>
     <p>
       Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-      inceptos himenaeos. Vestibulum ante ipsum primis in faucibus orci luctus
+      inceptos himenaeos. <a href="https://github.com/UzziahOsia/Naija-Spark">Vestibulum</a> ante ipsum primis in faucibus orci luctus
       et ultrices posuere cubilia curae; Duis consequat nulla ac ex consequat,
       in efficitur arcu congue. Nam fermentum commodo egestas.
     </p>
