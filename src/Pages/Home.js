70bd023d8c7e3a68c0d72437225c751ff71177ba
Uzziah Osia <85574539+UzziahOsia@users.noreply.github.com>
2023-04-12 11:59:12 +0100

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,9 +5,12 @@ const Home = () => (
     <h1 className="title is-1">Naija Spark</h1>
     <p>
       Discover your next favorite song with our music recommender app.
-      \nOur algorithm analyzes your listening habits and suggests new tracks tailored to your tastes.
-      \nWith a vast library of songs from different genres and eras, you'll never run out of options.
-      \nSave your favorite tracks and create playlists for any occasion. Start exploring new music today with our app.
+    </p>
+      Our algorithm analyzes your listening habits and suggests new tracks tailored to your tastes.
+    </p>
+      With a vast library of songs from different genres and eras, you'll never run out of options.
+    </p>
+    Save your favorite tracks and create playlists for any occasion. Start exploring new music today with our app.
     </p>
   </div>
 );
