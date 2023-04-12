7a4ff87bd4fc95e4b0c662deda381d3fcc334352
Uzziah Osia <85574539+UzziahOsia@users.noreply.github.com>
2023-04-12 11:35:39 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,20 +11,7 @@ const Navbar = ({ history }) => {
       aria-label="main navigation"
     >
       <div className="container">
-        <div className="navbar-brand">
-          <a
-            role="button"
-            className={`navbar-burger burger ${isOpen && "is-active"}`}
-            aria-label="menu"
-            aria-expanded="false"
-            onClick={() => setOpen(!isOpen)}
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
-
+        
         <div className={`navbar-menu ${isOpen && "is-active"}`}>
           <div className="navbar-start">
             <NavLink
