f100795d6cd33c49fd3acf269598686e118b2446
Uzziah Osia <85574539+UzziahOsia@users.noreply.github.com>
2023-04-12 15:17:53 +0100

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -14,17 +14,6 @@ const Home = () => (
     </p>
     <p>
       Save your favorite tracks and create playlists for any occasion. Start exploring new music today with our app.
-    </p>
-    <p>
-    </p>
-    <p>
-    </p>
-    <p>
-    </p>
-    <p> 
-    </p>
-    <p> 
-    </p>
     <p>
     Created by Uzziah Osia and Samuel Amiaka  
     </p>
