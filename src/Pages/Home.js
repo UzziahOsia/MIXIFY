88d18a9c3ad33fbe516eee7a836af01e805b3e70
Uzziah Osia <85574539+UzziahOsia@users.noreply.github.com>
2023-04-12 11:51:09 +0100

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,12 +3,12 @@ import React from "react";
 const Home = () => (
   <div>
     <h1 className="title is-1">Naija Spark</h1>
-    <h3>
-      Discover your next favorite song with our music recommender app. 
-      Our algorithm analyzes your listening habits and suggests new tracks tailored to your tastes.
-      With a vast library of songs from different genres and eras, you'll never run out of options. 
-      Save your favorite tracks and create playlists for any occasion. Start exploring new music today with our app.
-    </h3>
+    <p>
+      <br>Discover your next favorite song with our music recommender app.</br> 
+      <br>Our algorithm analyzes your listening habits and suggests new tracks tailored to your tastes.</br>
+      <br>With a vast library of songs from different genres and eras, you'll never run out of options.</br>
+      <br>Save your favorite tracks and create playlists for any occasion. Start exploring new music today with our app.</br>
+    </p>
   </div>
 );
 
