ed2436019141daeabdf3302e7aa6dd29312b1841
Uzziah Osia <85574539+UzziahOsia@users.noreply.github.com>
2023-04-12 15:01:05 +0100

    Update About.js

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,10 +4,28 @@ const About = () => (
   <div>
     <h1 className="title is-1">This is the About Page</h1>
     <p>
-      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-      inceptos himenaeos. <a href="https://github.com/UzziahOsia/Naija-Spark">Vestibulum</a> ante ipsum primis in faucibus orci luctus
-      et ultrices posuere cubilia curae; Duis consequat nulla ac ex consequat,
-      in efficitur arcu congue. Nam fermentum commodo egestas.
+      Music has been a constant companion in my life, offering solace and comfort in times of despair and joy in moments of celebration. It's been my go-to source for inspiration, motivation, and emotional release. Whenever I'm feeling down or overwhelmed, a good tune has the power to lift me up and make me feel alive again.
+      Music has helped me through some of the darkest moments of my life. When I lost a loved one, music was the only thing that could ease the pain and make me feel like everything was going to be okay. It helped me process my grief and find the strength to move forward.
+      In times of stress and anxiety, music has been my escape. It's provided a space where I can disconnect from the world and focus on the present moment. I've lost myself in the rhythms and melodies of songs, and in doing so, I've found a sense of calm and clarity.
+      But music isn't just a tool for coping with negative emotions. It's also been a source of joy and inspiration. It's helped me connect with others, forge new friendships, and explore new cultures. I've discovered artists and genres that have expanded my musical horizons and enriched my life.
+      Overall, music has been an integral part of my life, and I can't imagine where I'd be without it. It's been there for me through thick and thin, and I'm grateful for its unwavering presence in my life. Music is not just an art form but a language that transcends words and speaks directly to our souls, and for that, I'll always be grateful.
+    </p>
+    <h4>Uzziah Osia</h4>
+    <p>
+      Linkedin: <a href="https://www.linkedin.com/in/uzziah-osia/">Click Here</a>
+    </p>
+    <p>
+      Twitter: <a href="https://twitter.com/UzziahOsia">Click Here</a>
+    </p>
+    <p>
+      Github: <a href="https://github.com/UzziahOsia/">Click Here</a>
+    </p>
+    <h4>Samuel Amiaka</h4>
+    <p>
+      Linkedin: <a href="https://www.linkedin.com/in/samuel-amiaka/">Click Here</a>
+    </p>
+    <p>
+      Github: <a href="https://github.com/SamuelAmiaka">Click Here</a>
     </p>
   </div>
 );
